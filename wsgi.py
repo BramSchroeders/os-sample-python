67a87eab83f5ee78c2f11d62b0965ebdad042043
BramSchroeders <bram.schroederss@gmail.com>
2019-01-29 18:47:28 +0100

    changed application entry point.

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,11 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 from api_resource import HelloWorldExample
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 if __name__ == "__main__":
-    application.run()
-    api = Api(application)
-    CORS(application)
-    api.add_resource(HelloWorldExample,"/")
-    application.run()
+    app.run()
+    application = Api(app)
+    CORS(app)
+    application.add_resource(HelloWorldExample,"/")
+    app.run()
