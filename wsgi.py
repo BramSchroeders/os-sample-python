4460bb0b926f46f6121955627d4c6bf5b0d386aa
BramSchroeders <bram.schroederss@gmail.com>
2019-01-29 18:55:17 +0100

    changes.

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,10 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 from api_resource import HelloWorldExample
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run()
-    application = Api(app)
-    CORS(app)
+    api = Api(application)
+    CORS(application)
     application.add_resource(HelloWorldExample,"/")
-    app.run()
+    application.run()
