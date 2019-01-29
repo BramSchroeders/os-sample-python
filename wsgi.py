55c5b5c65c3a42d5a2c31c5ee496fd92d29ef056
BramSchroeders <bram.schroederss@gmail.com>
2019-01-29 20:27:26 +0100

    base example.

--- wsgi.py
+++ wsgi.py
@@ -8,13 +8,15 @@ api = Api(application)
 
 class HelloWorld(Resource):
     def get(self):
+        '''
         client = MongoClient('mongodb://172.30.197.160:27017/', username="database-user", password="database-password")
         db = client.test_database
         collection = db.test_collection
         res = collection.find_one()
         client.close()
         return res
-        #return {'hello': 'world'}
+        '''
+        return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/hello')
 
