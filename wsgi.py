dd72e1567a7a2c47fec9a50931b7135e047ee8eb
BramSchroeders <bram.schroederss@gmail.com>
2019-01-29 19:58:34 +0100

    restful

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,27 @@
 from flask_restful import Resource, Api
 from flask import Flask
+from pymongo import MongoClient
+
 application = Flask(__name__)
 api = Api(application)
 
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        client = MongoClient('mongodb://172.30.197.160:27017/')
+        db = client.test_database
+        collection = db.test_collection
+        res = collection.find_one()
+        client.close()
+        return res
+        #return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/hello')
 
 if __name__ == "__main__":
+    client = MongoClient('mongodb://172.30.197.160:27017/')
+    db = client.test_database
+    collection = db.test_collection
+    collection.insert_one({"message":"initial message"})
+    client.close()
     application.run()
