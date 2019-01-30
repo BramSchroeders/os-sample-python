8024eac2170c89a9bb7b8adf1d19eae8aabb9331
BramSchroeders <bram.schroederss@gmail.com>
2019-01-30 20:17:21 +0100

    mongodb

--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,29 @@
 from flask_restful import Resource, Api
 from flask import Flask
 from pymongo import MongoClient
+import os
 
 application = Flask(__name__)
 api = Api(application)
 
 
 class HelloWorld(Resource):
+
     def get(self):
-        
-        try:
-            client = MongoClient('mongodb://172.30.197.160:27017/', username="database-user", password="database-password")
-            db = client.test_database
-            collection = db.test_collection
-            result = {"result" : list(collection.find())}
-            return result
-        except Exception as e:
-            return {"msg":e.message}
-        
+        client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+        dbs = client.db
+        collection = dbs.test_collection
+        out = collection.find_one()
+        client.close()
+        return out
         #return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/hello')
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://172.30.197.160:27017/', username="database-user", password="database-password")
-    db = client.test_database
-    collection = db.test_collection
+    client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+    dbs = client.db
+    collection = dbs.test_collection
     collection.insert_one({"message":"initial message"})
     client.close()
     application.run()
