8655da632d2ba03293cd57d297ae8d2fbcd3d4b9
BramSchroeders <bram.schroederss@gmail.com>
2019-01-30 20:35:42 +0100

    mongodb

--- wsgi.py
+++ wsgi.py
@@ -10,15 +10,15 @@ api = Api(application)
 class HelloWorld(Resource):
 
     def get(self):
-        client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
-        client.close()
+        #client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+        #client.close()
         '''
         dbs = client.db
         collection = dbs.test_collection
         out = collection.find_one()
         client.close()
         '''
-        return {"hello":"world"}
+        return os.environ['OPENSHIFT_NOSQL_DB_URL']
         #return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/hello')
