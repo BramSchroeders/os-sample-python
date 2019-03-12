550d8be5716a8d0a18609207425118d586f09859
BramSchroeders <bram.schroederss@gmail.com>
2019-03-12 20:41:49 +0100

    added no cache headers.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import datetime
 
 application = Flask(__name__)
 api = Api(application)
@@ -15,7 +16,7 @@ class HelloWorld(Resource):
     def post(self):
         args = parser.parse_args()
         task = {'task' : args["task"]}
-        return task
+        return task, 200, {'Last-Modified': datetime.now(), 'Cache-Control' : 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0', 'Pragma': 'no-cache','Expires':'-1'}
 
 api.add_resource(HelloWorld, '/')
 
