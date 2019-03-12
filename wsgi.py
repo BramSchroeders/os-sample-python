bdc47484b510b845cb269bdbfc03ea8d8ace7794
BramSchroeders <bram.schroederss@gmail.com>
2019-03-12 20:05:10 +0100

    restful.

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,14 @@ from flask_restful import Resource, Api, reqparse
 application = Flask(__name__)
 api = Api(application)
 
-parser = reqparse.RequestParser()
-parser.add_argument('rate', type=int, help='Rate to charge for this resource')
-
 class HelloWorld(Resource):
 
     def get(self):
         return {'rate': 55}
     
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('rate', type=int, help='Rate to charge for this resource')  
         args = parser.parse_args()
         return {"found" : args['rate']}
 
