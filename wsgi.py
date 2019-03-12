fd3bc01309e6a9a53bccfc1bbfe38cc9f1c9609a
BramSchroeders <bram.schroederss@gmail.com>
2019-03-12 20:50:38 +0100

    added no cache headers.

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ class HelloWorld(Resource):
     def post(self):
         args = parser.parse_args()
         task = {'task' : args["task"]}
-        return task, 200, {'Last-Modified': datetime.now(), 'Cache-Control' : 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0', 'Pragma': 'no-cache','Expires':'-1'}
+        return task, 200#, {'Last-Modified': datetime.now(), 'Cache-Control' : 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0', 'Pragma': 'no-cache','Expires':'-1'}
 
 api.add_resource(HelloWorld, '/')
 
