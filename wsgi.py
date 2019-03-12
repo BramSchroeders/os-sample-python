dbaa5d00a9326e0320d53dfffe8f582448c61cad
BramSchroeders <bram.schroederss@gmail.com>
2019-03-12 19:55:50 +0100

    hello

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 application = Flask(__name__)
 api = Api(application)
 
 class HelloWorld(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('rate', type=int, help='Rate to charge for this resource')
+
     def get(self):
-        return {'hello': 'world'}
-        
+        return {'rate': 502}
+    
+    def post(self):
+        args = parser.parse_args()
+        return {"found" : args['rate']}
+
 api.add_resource(HelloWorld, '/')
 
 if __name__ == "__main__":
