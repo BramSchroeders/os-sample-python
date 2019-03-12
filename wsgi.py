6c0a93d443324a6db46872e0730dbb8b4c145b07
BramSchroeders <bram.schroederss@gmail.com>
2019-03-13 00:12:13 +0100

    added heroes.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+import psycopg2
 
 application = Flask(__name__)
 CORS(application)
@@ -19,7 +20,26 @@ class HelloWorld(Resource):
         task = {'task' : args["task"]}
         return task, 200#, {'Last-Modified': datetime.now(), 'Cache-Control' : 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0', 'Pragma': 'no-cache','Expires':'-1'}
 
+
+class HelloWorld2(Resource):
+
+    def get(self):
+        conn = psycopg2.connect("dbname=sampledb user=user0MQ password=KQoVIps3xkOSLQ7t")
+        cur = conn.cursor()
+        cur.execute('SELECT version()')
+        ver = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        return ver
+    
+    def post(self):
+        args = parser.parse_args()
+        task = {'task' : args["task"]}
+        return task, 200#, {'Last-Modified': datetime.now(), 'Cache-Control' : 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0', 'Pragma': 'no-cache','Expires':'-1'}
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld2, '/help/')
 
 if __name__ == "__main__":
     application.run()
